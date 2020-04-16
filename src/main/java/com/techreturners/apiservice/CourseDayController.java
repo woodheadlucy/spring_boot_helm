83e7b2f67d4af2a7eb5a6e2ee6df5123a10c0cfa
James Heggs <james.heggs@techreturners.com>
2020-04-16 18:36:10 +0100

    Add some standard logging

--- src/main/java/com/techreturners/apiservice/CourseDayController.java
+++ src/main/java/com/techreturners/apiservice/CourseDayController.java
@@ -1,5 +1,7 @@
 package com.techreturners.apiservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,11 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CourseDayController {
 
+    Logger logger = LoggerFactory.getLogger(CourseDayController.class);
+
     @Autowired
     protected CourseDayService courseDayService = null;
 
 	@GetMapping("/course-day")
 	public CourseDay getCourseDay(@RequestParam(defaultValue = "1") String day) {
-        return courseDayService.getDay(Integer.parseInt(day));
+        logger.info("getCourseDay() - START");
+        CourseDay courseDay = courseDayService.getDay(Integer.parseInt(day));
+        logger.info("getCourseDay() - END");
+        return courseDay;
 	}
 }
